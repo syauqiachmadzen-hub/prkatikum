66df6308cbb56cd10c9b8c53baafac6263c87a7d
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-09-21 11:48:32 +0700

    ControlStructure 1

---
+++ ControlStructure_133.py
@@ -0,0 +1,12 @@
+nilai = int(input("masukkan nilai: "))
+
+if nilai >= 90:
+    print ("Excellent performance")
+elif nilai >= 80:
+    print("Very Good performance")
+elif nilai >= 70:
+    print("Good performance")        
+elif nilai >= 60:
+    print("average performance") 
+else:
+    print("poor performance")       

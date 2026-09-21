1fd28452123f8e5866bc610d1c1afbf75e237181
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-09-21 10:56:47 +0700

    first commit

---
+++ meeting2.py
@@ -0,0 +1,43 @@
+answer=input("Would you like express shipping?")
+if answer == "yes":
+    print("That will be an extra $10.")
+
+
+duit=input("Masukkan jumlah duit Anda: ")
+duit = int(duit)
+if duit < 100 :
+    print("Miskin kamu homeless sivilis hiv.")
+
+duit = 150
+if duit > 100:
+    print("You have enough money to make a JAWA.")
+
+import turtle
+turtle.color('green')
+turtle.forward(100)
+turtle.right(90)
+turtle.color('blue')
+turtle.forward(100)
+turtle.right(90)
+turtle.color('pink')
+turtle.forward(100)
+
+import turtle
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+import turtle
+nbrSides = 10
+for steps in range(nbrSides):
+    turtle.forward(100)
+    turtle.right(360/nbrSides)
+    for moresteps in range(nbrSides):
+        turtle.forward(50)
+        turtle.right(360/nbrSides)
---
+++ prak2.py
@@ -0,0 +1,14 @@
+nilai = int(input("masukkan nilai: "))
+
+if nilai >= 90:
+    print ("exellent")
+elif nilai >= 80:
+    print("very good")
+elif nilai >= 70:
+    print("good")        
+elif nilai >= 60:
+    print("avrage") 
+else:
+    print("poor")       
+
+
